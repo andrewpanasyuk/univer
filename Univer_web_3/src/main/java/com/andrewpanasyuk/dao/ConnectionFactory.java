5ab99403db9f73a1c41920bd618b97f7edc8859f
Andrew Panasyuk <avpk77@gmail.com>
2017-03-26 13:33:19 +0300

    dev

--- Univer_web_3/src/main/java/com/andrewpanasyuk/dao/ConnectionFactory.java
+++ Univer_web_3/src/main/java/com/andrewpanasyuk/dao/ConnectionFactory.java
@@ -1,8 +1,13 @@
 package com.andrewpanasyuk.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConnectionFactory {
 	private static ConnectionFactory instance = new ConnectionFactory();
@@ -10,6 +15,8 @@ public class ConnectionFactory {
 	private static final String PASS = "root";
 	private static final String USER = "postgres";
 	private static final String DRIVER = "org.postgresql.Driver";
+	@Resource(name="jdbc/UniverDB")
+	private DataSource source;
 	
 	private ConnectionFactory(){
 		try {
@@ -19,10 +26,11 @@ public class ConnectionFactory {
 		}
 	}
 	
-	private Connection createConnection(){
+	private Connection createConnection() throws NamingException{
+		Context context = new InitialContext();
 		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection(URL, USER, PASS);
+			connection = source.getConnection();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -31,7 +39,13 @@ public class ConnectionFactory {
 	
 
 	public static Connection getConnect() {
-		return instance.createConnection();
+		try {
+			return instance.createConnection();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
